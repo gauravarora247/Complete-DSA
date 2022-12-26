04c84d4fc9fd998eaf4998486003726d075ea9bd
gaurav <gauravaroradav@gmail.com>
2022-12-26 23:07:44 +0530

    pattern ques

--- basics/patterns.cpp
+++ basics/patterns.cpp
@@ -1,16 +1,59 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cout<<"Enter the value of n "<<endl;
+//     cin>>n;
+// for(int i = 0 ;i<=n;i++)
+// {
+//     for(int j=0;j<=n;j++)
+//     {
+//         cout<<"*\t";
+//     }
+//     cout<<endl;
+// }
+// }
+
+//pattern 1 
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+// int n;
+// cout<<"Enter the value of n "<<endl;
+// cin>>n;
+// for(int i=1;i<=n;i++)
+// {
+//     for(int j=1;j<=n;j++)
+//     {
+//         cout<<i<<"\t";
+//     }
+//     cout<<endl;
+// }
+// }
+
+//pattern 2 
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int n;
-    cout<<"Enter the value of n "<<endl;
-    cin>>n;
-for(int i = 0 ;i<=n;i++)
+int n;
+int m;
+
+cout<<"Enter the value of n"<<endl;
+cin>>n;
+cout<<"Enter the value of m"<<endl;
+cin>>m;
+for(int i=1;i<=n;i++)
 {
-    for(int j=0;j<=n;j++)
+    for(int j=1;j<=m;j++)
     {
-        cout<<"*\t";
+        cout<<j<<"\t";
+
     }
     cout<<endl;
 }
 }
+
+//
