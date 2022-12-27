2ce3f96a0e4fbedf362d047f0448566d774217eb
gaurav <gauravaroradav@gmail.com>
2022-12-27 22:41:34 +0530

    pattern 9

--- basics/patterns.cpp
+++ basics/patterns.cpp
@@ -164,21 +164,42 @@
 // }
 
 //pattern 8
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+// int n;
+// cout<<"Enter the value of n"<<endl;
+// cin>>n;
+// for(int i=1;i<=n;i++)
+// {
+//     int value = i;
+//     for(int j=1;j<=i;j++)
+//     {
+//         cout<<value<<"\t";
+//         value++;
+//     }
+//     cout<<endl;
+// }
+// }
+
+
+// pattern 9
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
 int n;
-cout<<"Enter the value of n"<<endl;
+cout<<"Enter the value of n "<<endl;
 cin>>n;
-for(int i=1;i<=n;i++)
-{
-    int value = i;
-    for(int j=1;j<=i;j++)
+    for(int i=1;i<=n;i++)
     {
-        cout<<value<<"\t";
-        value++;
+        int value = i ;
+        for(int j=1;j<=i;j++)
+        {
+            cout<<value<<"\t";
+            value--;
+        }
+        cout<<endl;
     }
-    cout<<endl;
-}
 }
