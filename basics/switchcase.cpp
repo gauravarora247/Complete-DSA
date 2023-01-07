196a31bf8bbaf1ee094785a8832b54ea0f52acd1
gaurav <gauravaroradav@gmail.com>
2023-01-07 23:10:21 +0530

    done

---
+++ basics/switchcase.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num = 2;
+
+    switch(num)
+    {
+        case 1:
+        cout<<"first"<<endl;
+        break;
+        case 2:
+        cout<<"Second "<<endl;
+        break;
+        default:
+        cout<<"It is default case "<<endl;
+         
+    }
+}
