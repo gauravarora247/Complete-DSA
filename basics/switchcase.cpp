c42f14fc673db150e17c6480c9058e5d77c44f66
gaurav <gauravaroradav@gmail.com>
2023-01-09 23:09:45 +0530

    status update

--- basics/switchcase.cpp
+++ basics/switchcase.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 int main()
 {
-    int num = 2;
+    // int num = 2;
+ char num = '1';
 
     switch(num)
     {
         case 1:
         cout<<"first"<<endl;
         break;
-        case 2:
+        case '1':
         cout<<"Second "<<endl;
         break;
         default:
@@ -17,3 +18,6 @@ int main()
          
     }
 }
+
+//default case is not necessary 
+// break is necessary to break the cases 
