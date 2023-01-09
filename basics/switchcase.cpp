e7455e211e435ad23c8b6045815b14c398d12cdb
gaurav <gauravaroradav@gmail.com>
2023-01-09 23:34:03 +0530

    changes done

--- basics/switchcase.cpp
+++ basics/switchcase.cpp
@@ -1,23 +1,101 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     // int num = 2;
+//  char ch = '1';
+//   int num = 1;
+//     switch(ch)
+//     {
+//         case 1:
+//         cout<<"first"<<endl;
+//         break;
+//         case '1':
+//         // cout<<"Second "<<endl;
+//            switch(num)
+//            {
+//             case 1:
+//             cout<<"Value of num is "<<" "<<num<<endl;
+//            }
+//         break;
+//         default:
+//         cout<<"It is default case "<<endl;
+         
+//     }
+// }
+
+// //default case is not necessary 
+// // break is necessary to break the cases 
+
+
+//Calculator using switch case 
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    // int num = 2;
- char num = '1';
+int n;
+int m;
+cout<<"Enter the value of n"<<endl;
+cin>>n;
+cout<<"Enter the value of m"<<endl;
+cin>>m;
 
-    switch(num)
+char op;
+cout<<"Enter the operation "<<endl;
+cin>>op;
+
+switch(op)
+{
+    case '+':
     {
-        case 1:
-        cout<<"first"<<endl;
-        break;
-        case '1':
-        cout<<"Second "<<endl;
-        break;
-        default:
-        cout<<"It is default case "<<endl;
-         
+      cout<<(n+m);
+      break;
     }
+    case '-':
+    {
+     if(n>m)
+     {
+        cout<<(n-m);
+     }
+     else
+     {
+        cout<<(m-n);
+     }
+     break;
+    }
+
+    case '*':
+    {
+      cout<<n*m<<endl;
+      break;
+    }
+
+    case '/':
+    {
+    if(n>m)
+    {
+        cout<<(n/m);
+    }
+    else
+    {
+        cout<<(m/n);
+    }
+    }
+    
+    case '%':
+    {
+     cout<<(n%m);
+    }
+    
+
+    
 }
 
-//default case is not necessary 
-// break is necessary to break the cases 
+
+
+
+
+
+
+}
